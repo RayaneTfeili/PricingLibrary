76181fb6a26ed34610a2dd82151cc96f4a75dde4
Rayane <rayantfeili@gmail.com>
2026-07-20 23:39:27 +0200

    feat : adding test for IC

--- test/test_MC.py
+++ test/test_MC.py
@@ -53,3 +53,40 @@ def test_mc_american_put_price_positive():
     mc = MCPricer(option, 100, 0.05, 0.2, 100_000, 100, 42)
 
     assert mc.price() > 0
+
+def test_mc_confidence_interval_contains_price():
+    option = VanillaOption("call", 100, 1)
+
+    mc = MCPricer(option, 100, 0.05, 0.2, 100_000, 252, 42)
+
+    lower, upper = mc.IC(0.95)
+    price = mc.price()
+
+    assert lower < price < upper
+
+def test_mc_confidence_interval_width_decreases():
+    option = VanillaOption("call", 100, 1)
+
+    mc_small = MCPricer(option, 100, 0.05, 0.2, 10_000, 252, 42)
+    mc_large = MCPricer(option, 100, 0.05, 0.2, 100_000, 252, 42)
+
+    lower_small, upper_small = mc_small.IC(0.95)
+    lower_large, upper_large = mc_large.IC(0.95)
+
+    width_small = upper_small - lower_small
+    width_large = upper_large - lower_large
+
+    assert width_large < width_small
+
+def test_mc_confidence_interval_99_is_wider_than_95():
+    option = VanillaOption("call", 100, 1)
+
+    mc = MCPricer(option, 100, 0.05, 0.2, 100_000, 252, 42)
+
+    lower_95, upper_95 = mc.IC(0.95)
+    lower_99, upper_99 = mc.IC(0.99)
+
+    width_95 = upper_95 - lower_95
+    width_99 = upper_99 - lower_99
+
+    assert width_99 > width_95
