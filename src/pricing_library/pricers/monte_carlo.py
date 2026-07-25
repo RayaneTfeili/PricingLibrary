91a04ab1b32f7a403f8a75e3838031c1e2f9f7d3
Rayane <rayantfeili@gmail.com>
2026-07-25 15:40:29 +0200

    feat : adding antithetic (need a fix tho)

--- src/pricing_library/pricers/monte_carlo.py
+++ src/pricing_library/pricers/monte_carlo.py
@@ -23,8 +23,23 @@ class MCPricer(BasePricer):
             z = rng.standard_normal(self.num_simulations)
             paths[:,j] = paths[:,j-1]*np.exp((R - 0.5*vol**2)*dt + vol*np.sqrt(dt)*z)
         return paths
-    
-    def discounted_payoffs(self):
+    def path_antithetic(self):
+        T = self.option.maturity 
+        S0 = self.spot_price 
+        R = self.risk_free_rate 
+        vol = self.volatility 
+        dt = T/self.nums_step 
+        rng = np.random.default_rng(self.seeds)
+        path1 = np.zeros([self.num_simulations, self.nums_step+1])
+        path2 = np.zeros([self.num_simulations, self.nums_step+1])
+        path1[:,0] = S0
+        path2[:,0]= S0 
+        for j in range(1,self.nums_step+1):
+            z=  rng.standard_normal(self.num_simulations)
+            path1[:,j] = path1[:,j-1] * np.exp(R-(vol**2)/2 * dt + vol *np.sqrt(dt)* z) 
+            path2[:,j] = path2[:,j-1] * np.exp(R-(vol**2)/2 * dt + vol *np.sqrt(dt)* -z)
+        return path1,path2 
+    def payoff_from_path(self,path):
         if isinstance(self.option, AmericanOption):
             raise TypeError("We are using Long-Scharwtz method for the american option")
         if isinstance(self.option, AsianOption):
@@ -34,9 +49,7 @@ class MCPricer(BasePricer):
             else:
                 average_prices = np.exp(np.mean(np.log(paths[:, 1:]), axis=1))
             payoffs = np.maximum(average_prices - self.option.strike, 0) if self.option.is_call() else np.maximum(self.option.strike - average_prices, 0)
-            discounted_payoff = np.exp(-self.risk_free_rate *self.option.maturity) *payoffs
-            return discounted_payoff
-
+            return payoffs
         if isinstance(self.option, BarrierOption):
             paths = self.path()
             final_prices = paths[:, -1]
@@ -52,21 +65,27 @@ class MCPricer(BasePricer):
             else:
                 barrier_hit = np.any(paths[:, 1:] <= self.option.barrier_level, axis=1)
                 active = ~barrier_hit
-
             payoffs = np.maximum(final_prices - self.option.strike, 0) if self.option.is_call() else np.maximum(self.option.strike - final_prices, 0)
             payoffs = np.where(active, payoffs, 0)
-
-            discounted_payoff = np.exp(-self.risk_free_rate * self.option.maturity)*payoffs
-            return discounted_payoff
-
+            return payoffs 
         if isinstance(self.option, VanillaOption):
             paths = self.path()
             final_prices = paths[:, -1]
             payoffs = np.maximum(final_prices - self.option.strike, 0) if self.option.is_call() else np.maximum(self.option.strike - final_prices, 0)
-            discounted_payoff = np.exp(-self.risk_free_rate * self.option.maturity) * payoffs
-
-            return discounted_payoff
+            return payoffs 
         raise TypeError("Swing Option coming in the futur, when I have more time")
+    
+    def discounted_payoffs(self):
+        paths = self.path()
+        payoff = self.payoff_from_path(paths)
+        return np.exp(-self.risk_free_rate*self.option.maturity)*payoff 
+    def discounted_payoffs_antithetic(self):
+        path1,path2 = self.path_antithetic()
+        payoff1 = self.payoff_from_path(path1)
+        payoff2 = self.payoff_from_path(path2)
+        payoff_antithetic = 0.5*(payoff1+payoff2)
+        return np.exp(-self.risk_free_rate*self.option.maturity)*payoff_antithetic
+        
 
     def _price_american(self):
             paths = self.path()
@@ -90,7 +109,11 @@ class MCPricer(BasePricer):
             return self._price_american()
         discounted_payoff= self.discounted_payoffs()
         return np.mean(discounted_payoff)
-
+    def price_antithetic(self):
+        if isinstance(self.option,AmericanOption):
+            return self._price_american()
+        discounted_payoff = self.discounted_payoffs_antithetic()
+        return np.mean(discounted_payoff)
     def IC(self, level : float):
         disct_payoff = self.discounted_payoffs()
         price = np.mean(disct_payoff)
@@ -98,7 +121,14 @@ class MCPricer(BasePricer):
         alpha = 1 - level
         z = norm.ppf(1-alpha/2)
         return [price - error * z, price + error * z]
-
+    def IC_antithetic(self,level : float):
+        disct_payoff = self.discounted_payoffs_antithetic()
+        price = np.mean(disct_payoff)
+        error = np.std(disct_payoff, ddof = 1)/np.sqrt(self.num_simulations)
+        alpha = 1-level 
+        z = norm.ppf(1-alpha/2)
+        return [price - error * z, price + error*z]
+    
     def delta(self, h : float = 0.01):
         bump_up = MCPricer(self.option, self.spot_price+h, self.risk_free_rate,self.volatility, self.num_simulations, self.nums_step,self.seeds).price()
         bump_down =  MCPricer(self.option, self.spot_price-h, self.risk_free_rate,self.volatility, self.num_simulations, self.nums_step,self.seeds).price()
